8ffb72093a4ba08786fa6639524bd864a4aa18d4
charbel sarkis <charbelsako@gmail.com>
2019-09-07 17:32:58 +0300

    Added templates for header and footer.

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,5 +1,51 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import ListItem from '@material-ui/core/ListItem';
+import List from '@material-ui/core/List';
+import ListItemText from '@material-ui/core/ListItemText';
+
+//colors
+import indigo from '@material-ui/core/colors/indigo';
+
+const styles = {
+  link: {
+    color: 'white',
+    textDecoration: 'none'
+  },
+  footer: {
+    backgroundColor: indigo[500],
+    padding: 20,
+    color: 'white'
+  },
+  listItem: { padding: 0 }
+};
 
 export default function Footer() {
-  return <footer></footer>;
+  return (
+    <div style={styles.footer}>
+      <Typography variant="subtitle2" style={{ textAlign: 'center' }}>
+        Copyright 2019
+      </Typography>
+      <List>
+        <ListItem style={styles.listItem}>
+          <ListItemText>
+            <Typography variant="subtitle2">
+              <a style={styles.link} href="https://github.com/charbelsako">
+                Github account
+              </a>
+            </Typography>
+          </ListItemText>
+        </ListItem>
+        <ListItem style={styles.listItem}>
+          <ListItemText>
+            <Typography variant="subtitle2">
+              <a style={styles.link} href="https://instagram.com/charbelsako">
+                Instagram account
+              </a>
+            </Typography>
+          </ListItemText>
+        </ListItem>
+      </List>
+    </div>
+  );
 }
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -6,18 +6,24 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItem from '@material-ui/core/ListItem';
 
 // Icon imports
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 import MenuIcon from '@material-ui/icons/Menu';
+import HomeIcon from '@material-ui/icons/Home';
+import ContactIcon from '@material-ui/icons/Contacts';
+import SchoolIcon from '@material-ui/icons/School';
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
+  hide: {
+    display: 'none'
+  },
   root: {
     flexGrow: 1
   },
@@ -27,14 +33,14 @@ const useStyles = makeStyles(theme => ({
       duration: theme.transitions.duration.leavingScreen
     })
   },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
+  // appBarShift: {
+  //   width: `calc(100% - ${drawerWidth}px)`,
+  //   marginLeft: drawerWidth,
+  //   transition: theme.transitions.create(['margin', 'width'], {
+  //     easing: theme.transitions.easing.easeOut,
+  //     duration: theme.transitions.duration.enteringScreen
+  //   })
+  // },
   drawer: {
     width: drawerWidth,
     flexShrink: 0
@@ -58,13 +64,13 @@ const useStyles = makeStyles(theme => ({
     }),
     marginLeft: -drawerWidth
   },
-  contentShift: {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    }),
-    marginLeft: 0
-  },
+  // contentShift: {
+  //   transition: theme.transitions.create('margin', {
+  //     easing: theme.transitions.easing.easeOut,
+  //     duration: theme.transitions.duration.enteringScreen
+  //   }),
+  //   marginLeft: 0
+  // },
   menuButton: {
     marginRight: theme.spacing(2)
   },
@@ -88,7 +94,7 @@ export default function Header() {
 
   return (
     <header>
-      <AppBar position="fixed" color="primary">
+      <AppBar position="fixed" color="primary" className={classes.appBar}>
         <Toolbar>
           <IconButton
             onClick={toggleNavigation}
@@ -102,7 +108,14 @@ export default function Header() {
           </Typography>
         </Toolbar>
       </AppBar>
-      <Drawer variant="temporary" anchor="left" open={open}>
+      <Drawer
+        variant="temporary"
+        anchor="left"
+        open={open}
+        className={classes.drawer}
+        classes={{
+          paper: classes.drawerPaper
+        }}>
         <div className={classes.drawerHeader}>
           <IconButton onClick={toggleNavigation}>
             {theme.direction === 'ltr' ? (
@@ -113,8 +126,17 @@ export default function Header() {
           </IconButton>
         </div>
         <List>
-          {['Home', 'Contact Me', ''].map((text, index) => (
+          {['Home', 'Contact Me', 'Certifications'].map((text, index) => (
             <ListItem button key={index}>
+              <ListItemIcon>
+                {text === 'Contact Me' ? (
+                  <ContactIcon />
+                ) : text === 'Certifications' ? (
+                  <SchoolIcon />
+                ) : (
+                  <HomeIcon />
+                )}
+              </ListItemIcon>
               <ListItemText primary={text} />
             </ListItem>
           ))}
