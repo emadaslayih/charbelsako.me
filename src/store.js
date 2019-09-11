d23344babfc7aed80d7a12556f9135681931d48d
charbel sarkis <charbelsako@gmail.com>
2019-09-11 18:26:15 +0300

    Added projects data to the store.

--- src/store.js
+++ src/store.js
@@ -2,9 +2,21 @@ import { createStore } from 'redux';
 
 const initialState = {
   projects: [
-    { title: 'title1', description: 'desc' },
-    { title: 'title1', description: 'desc' },
-    { title: 'title1', description: 'desc' }
+    {
+      title: 'Dev Connector',
+      description: `A website that allows developers to communicate with
+        each other and make an online portfolio.`,
+      live: 'https://hidden-bayou-89767.herokuapp.com/',
+      code: 'https://github.com/charbelsako/devnetwork'
+    },
+    {
+      title: 'Client Panel',
+      description: `This project is intended to help users that need to keep track of what
+        money they owe people, and vice versa.`,
+      live: 'https://react-client-panel-afaa7.firebaseapp.com',
+      code: 'https://github.com/charbelsako/client-panel'
+    }
+    // { title: 'title1', description: 'desc' }
   ],
   skills: [
     'PHP',
