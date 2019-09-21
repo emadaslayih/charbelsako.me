977415030eba88ad0305034177e0c44ec6485cdc
charbel sarkis <charbelsako@gmail.com>
2019-09-21 20:54:37 +0300

    Added an abstraction class for the native, TextField component.

---
+++ src/components/common/InputTextField.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import TextField from '@material-ui/core/TextField'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import AccountCircle from '@material-ui/icons/AccountCircle'
+import Email from '@material-ui/icons/Email'
+import Comment from '@material-ui/icons/Comment'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  margin: {
+    margin: theme.spacing(1)
+  }
+}))
+
+export default function InputTextField(props) {
+  const classes = useStyles()
+
+  return (
+    <TextField
+      value={props.value}
+      id={props.id}
+      type={props.type}
+      onChange={props.onChange}
+      className={classes.margin}
+      label={props.label}
+      multiline={props.multiline}
+      rows={4}
+      variant="outlined"
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            {props.icon === 'account' && <AccountCircle />}
+            {props.icon === 'email' && <Email />}
+            {props.icon === 'comment' && <Comment />}
+          </InputAdornment>
+        )
+      }}
+    />
+  )
+}
