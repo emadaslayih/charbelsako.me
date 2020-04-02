5769340922cf471f823e9861626e6c66de2a5a75
Charbel Sarkis <charbelsako@gmail.com>
2020-04-02 16:46:41 +0300

    Removed an image from the homepage. Will be adjusted then added again

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,14 @@
-import React from "react"
+import React from 'react';
 
-import ProjectList from "./ProjectList"
+import ProjectList from './ProjectList';
 // import CourseList from './CourseList';
-import SkillList from "./SkillList"
-import Experience from "./Experience"
-import BackgroundImage from "./BackgroundImage"
+import SkillList from './SkillList';
+import Experience from './Experience';
+import BackgroundImage from './BackgroundImage';
 
-import image2 from "../images/image1.jpg"
-import image3 from "../images/image2.jpg"
-import styles from "./styles"
+// import image2 from '../images/image1.jpg';
+import image3 from '../images/image2.jpg';
+import styles from './styles';
 
 export default function Home() {
   return (
@@ -16,9 +16,9 @@ export default function Home() {
       <BackgroundImage image={image3} />
       <Experience />
       <ProjectList />
-      <BackgroundImage image={image2} />
+      {/* <BackgroundImage image={image2} /> */}
       <SkillList />
       {/* <CourseList /> */}
     </div>
-  )
+  );
 }
